21843ed733917882898eaecb375c0dd1dd095d9b
Ulisses Genguini <ulissesgenguini@Ulissess-MacBook-Air.local>
2023-03-14 07:26:29 -0300

    inicial commit

--- App.js
+++ App.js
@@ -1,10 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar } from 'expo-status-bar'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const [previsao, setPrevisao] = useState()
+
+  useEffect(() => {
+    buscaPrevisao('pirulito')
+  }, [])
+
+
+  function buscaPrevisao() {
+    axios.get('https://api.open-meteo.com/v1/forecast?latitude=24.3&longitude=52&current_weather=true&hourly=temperature_2m')
+      .then(function (res) {
+        console.log(res.data)
+        setPrevisao(res.data)
+      })
+      .catch(function (error) {
+        console.log(error)
+      })
+  }
+  
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ fontSize: 100}}>{previsao?.current_weather?.temperature} ºC</Text>
       <StatusBar style="auto" />
     </View>
   );
